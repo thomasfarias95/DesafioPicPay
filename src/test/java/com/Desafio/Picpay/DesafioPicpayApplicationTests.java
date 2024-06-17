946ccefeb13fe9f2038c0abe6aa307359df6f213
Thomas Farias <thomasfarias0995@gmail.com>
2024-06-17 20:50:19 -0300

    feat: initial commit

---
+++ src/test/java/com/Desafio/Picpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Desafio.Picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
